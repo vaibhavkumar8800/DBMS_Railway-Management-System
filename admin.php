8075ec844da324fab6f352d3273d98538760d9ac
vaibhavkumar8800 <85095173+vaibhavkumar8800@users.noreply.github.com>
2021-05-31 11:57:13 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,18 @@
+<div class="main"><div class="pass"><a href="addpass.php">ADD PASSENGER</a></div><br>
+<div class="pass"><a href="addtrain.php">ADD TRAIN</a></div></div>
+<style>
+    .main{
+        justify-content:center;
+        align:center;
+    }
+    .pass{
+        text-decoration:bold;
+    height:100px;
+    width:80%;
+   align-item:center;
+    font-size:24px;
+    color:lightgreen;
+    background-color:lightblue;
+    
+}
+    </style>
